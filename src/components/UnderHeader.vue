505b8f3346f5a976764fc0953e884d7bfc701533
Vladimir Horev <VShorev@yandex.ru>
2023-06-24 17:05:26 +0300

    6

--- src/components/UnderHeader.vue
+++ src/components/UnderHeader.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="d-flex justify-content-between mx-5 ">
-    <button type="button" class="btn btn-secondary">Добавить проект</button>
-    <button type="button" class="btn btn-secondary">Выход</button>
+    <button type="button" class="btn btns btn-secondary link-light link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover"> Добавить проект</button>
+    <button type="button" class="btn btn-secondary "><a href="https://report-dev.uat.sibcode.team/login" class="link-light link-offset-2 link-underline-opacity-0 link-underline-opacity-100-hover">Выход</a> </button>
   </div>
+
+  
 </template>
+
+
+<style>
+  @import '../css/style.css';
+</style>
